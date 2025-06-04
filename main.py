0655ad96c104fb44f37f415e3cd269d4953c38c7
Sumit <sumit13thakur124@gmail.com>
2025-06-04 15:07:08 +0530

    Completed Data_Transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Network_Security.components.data_ingestion import DataIngestion
 from Network_Security.exception.exception import CustomException
 from Network_Security.components.data_validation import DataValidation
+from Network_Security.components.data_transformation import DataTransformation,DataTransformationConfig
 from Network_Security.logging.logger import logging
 from Network_Security.entity.config_entity import (
     DataIngestionConfig,
@@ -21,6 +22,8 @@ if __name__ == "__main__":
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Ingestion completed")
         print(data_ingestion_artifact)
+
+
         data_validation_config = DataValidationConfig(
             training_pipeline_config=training_pipeline_config
         )
@@ -32,6 +35,16 @@ if __name__ == "__main__":
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation completed")
         print(data_validation_artifact)
+
+        logging.info("datatransformation started")
+        data_transformation_config = DataTransformationConfig(
+            training_pipeline_config=training_pipeline_config
+        )
+        data_transformation_artifact = DataTransformation(data_transformation_config=data_transformation_config,data_validation_artifact=data_validation_artifact)
+        data_transformation_artifact = data_transformation_artifact.initiate_data_transformation()
+        logging.info("datatransformation completed")
+        print(data_transformation_artifact)
+        
         
     except Exception as e:
         raise CustomException(e, sys)
